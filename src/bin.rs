e62931fd082f4edb81b831fb3e372925d802e316
Theodore Keloglou <zf@sirodoht.com>
2023-05-18 03:06:46 +0100

    add port as env variable

--- src/bin.rs
+++ src/bin.rs
@@ -2,14 +2,21 @@
 use std::net::SocketAddr;
 
 #[cfg(feature = "local")]
+use std::env;
+
+#[cfg(feature = "local")]
 use anyhow::Result;
 
 #[cfg(feature = "local")]
 #[tokio::main]
 async fn main() -> Result<()> {
     let router = zerocal::get_router();
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
-    println!("Listening on http://localhost:8000/");
+    let port: u16 = env::var("PORT")
+        .unwrap_or_else(|_| String::from("8000"))
+        .parse()
+        .expect("PORT must be a number");
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    println!("Listening on http://localhost:{}/", port);
     axum::Server::bind(&addr)
         .serve(router.into_make_service())
         .await?;
